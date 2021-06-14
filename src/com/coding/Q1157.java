aa73358969a65455646941fa0e71a8b988da35c5
SeungWonKwon <loveyoo0103@naver.com>
2021-06-14 15:22:38 +0900

    Q1157.java

--- src/com/coding/Q1157.java
+++ src/com/coding/Q1157.java
@@ -10,24 +10,46 @@ import java.util.Set;
 public class Q1157 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String originWord = bufferedReader.readLine();
-        String[] splitWord = originWord.split("");
-        Set<String> set = new HashSet<>(Arrays.asList(splitWord));
-        int result = 0;
-        String same = "";
-        for(String a : set) {
-            int count = originWord.length() - originWord.replace(a, "").length();
-            if(count > result) {
-                result = count;
-            } else if(count == result) {
-                same = "?";
+        String originWord = bufferedReader.readLine().toUpperCase();
+        int max = 0;
+        String result = "";
+        Set<String> overlapWord = new HashSet<>(Arrays.asList(originWord.split("")));
+        for(String word : overlapWord) {
+            int count = originWord.length() - originWord.replace(word, "").length();
+            if(count > max) {
+                result = word;
+                max = count;
+            } else if(count == max) {
+                result = "?";
             }
         }
-        if(same.length() > 0) {
-            System.out.println(result);
-        } else {
-            System.out.println(same);
-        }
+        System.out.println(result);
         bufferedReader.close();
     }
+
+    /* 정답코드
+    int[] arr = new int[26];
+    int c = System.in.read();
+        while (c > 64) {
+        if (c < 91) {
+            arr[c - 65]++;
+        } else {
+            arr[c - 97]++;
+        }
+        c = System.in.read();
+    }
+    int max = -1;
+    int ch = -2;
+        for (int i = 0; i < 26; i++) {
+
+        if (arr[i] > max) {
+            max = arr[i];
+            ch = i;
+        } else if (arr[i] == max) {
+            ch = -2;
+        }
+    }
+        System.out.print((char) (ch+65));
+        
+     */
 }
