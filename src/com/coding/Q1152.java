13d9c93dbe449e9bc19cb702728885003931a742
SeungWonKwon <loveyoo0103@naver.com>
2021-08-06 19:33:21 +0900

    Q1152 백준 풀이

---
+++ src/com/coding/Q1152.java
@@ -0,0 +1,13 @@
+package com.coding;
+
+import java.io.*;
+
+public class Q1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String target = reader.readLine().trim();
+        String[] item = target.split(" ");
+        System.out.println(target.isEmpty() ? 0 : item.length);
+        reader.close();
+    }
+}
