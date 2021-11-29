35d16823c5894be20ad08166e52ad67213161313
SeungWonKwon <loveyoo0103@naver.com>
2021-11-29 14:51:54 +0900

    Q10818 최소,최대

---
+++ src/com/coding/Q10818.java
@@ -0,0 +1,20 @@
+package com.coding;
+import java.io.*;
+import java.util.Arrays;
+
+public class Q10818 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        reader.readLine();
+        String[] value = reader.readLine().split(" ");
+        int[] result = Arrays.asList(value).stream().mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(result);
+        System.out.println(result[0] + " " + result[result.length -1]);
+
+//		while((str = reader.readLine()) != null) {
+//		}
+//		writer.close();
+    }
+}
