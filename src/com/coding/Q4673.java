804b7ddd963f5d4b15fb79ddbf989471642dc3f1
seungwon-kwon <loveyoo0103@naver.com>
2021-11-29 22:49:08 +0900

    Q1152 백준 풀이

--- src/com/coding/Q4673.java
+++ src/com/coding/Q4673.java
@@ -1,29 +1,34 @@
 package com.coding;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 //셀프넘버
 public class Q4673 {
     public static void main(String[] args) throws IOException {
 //        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] numbers = {};
-        for(int i = 1; i <= 1000; i++) {
+        int[] numbers = new int[10001];
+        for(int i = 1; i <= 10000; i++) {
             numbers[i] = i;
         }
-        for(int i = 1; i <= 1000; i++) {
+        for(int i = 1; i <= 10000; i++) {
             String[] splitNumber = String.valueOf(i).split("");
             int sum = 0;
             for(int j = 0 ; j < splitNumber.length; j++) {
                 sum += i + Integer.parseInt(splitNumber[j]);
             }
-            numbers[sum] = Integer.parseInt(null);
+            if(sum < 10000) {
+                numbers[sum] = 0;
+            }
         }
-        for(int i = 0; i < )
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] != 0) {
+                writer.append(numbers[i] + "\n");
+            }
+        }
         writer.flush();
         writer.close();
     }
