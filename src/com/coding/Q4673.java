72b744427b61eed78e5c697e62ef119da82c418d
SeungWonKwon <loveyoo0103@naver.com>
2021-11-30 18:19:27 +0900

    백준 등차수열, 셀프넘버

--- src/com/coding/Q4673.java
+++ src/com/coding/Q4673.java
@@ -3,22 +3,21 @@ package com.coding;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 
 //셀프넘버
 public class Q4673 {
     public static void main(String[] args) throws IOException {
 //        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] numbers = new int[10001];
-        for(int i = 1; i <= 10000; i++) {
+        int[] numbers = new int[10000];
+        for(int i = 1; i < 10000; i++) {
             numbers[i] = i;
         }
-        for(int i = 1; i <= 10000; i++) {
+        for(int i = 1; i < 10000; i++) {
             String[] splitNumber = String.valueOf(i).split("");
-            int sum = 0;
+            int sum = i;
             for(int j = 0 ; j < splitNumber.length; j++) {
-                sum += i + Integer.parseInt(splitNumber[j]);
+                sum += Integer.parseInt(splitNumber[j]);
             }
             if(sum < 10000) {
                 numbers[sum] = 0;
