4e2514b92fce498ef7071dcdfbe747ab1610ac28
SeungWonKwon <loveyoo0103@naver.com>
2021-12-07 18:21:28 +0900

    백준 설탕배달 푸는중

--- src/com/coding/Q2839.java
+++ src/com/coding/Q2839.java
@@ -13,13 +13,30 @@ public class Q2839 {
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int value = Integer.parseInt(reader.readLine());
-        System.out.println(value / 5);
-        System.out.println(value / 3);
-        if(value % 5 < 0 || value % 3 < 0) {
-            writer.append("-1");
+        int quoFive = value / 5;
+        int reFive = value % 5;
+        int quoThree = value / 3;
+        int divThree = value % 3;
+        if (quoFive > 0 && reFive == 0) { //5로 나눠떨어질 때
+            writer.append(String.valueOf(quoFive));
+        } else if(quoFive > 0 && reFive % 3 == 0) { // 5로 나눈 나머지가 3으로 나눠떨어질 때
+            writer.append(String.valueOf(quoFive + (reFive / 3)));
+        } else if(quoThree > 0 && divThree == 0) { // 3으로 나눠떨어질 때
+            writer.append(String.valueOf(quoThree));
+        } else { // 외의 상황
+            int count = 0;
+            while(true) {
+                count++;
+                value -= 5;
+                if(value < 3) {
+                    writer.append("-1");
+                    break;
+                } else if (value % 3 == 0) {
+                    writer.append(String.valueOf(count + (value / 3)));
+                    break;
+                }
+            }
         }
-
-
 
         writer.flush();
         writer.close();
--- src/com/coding/Q4673.java
+++ src/com/coding/Q4673.java
@@ -10,21 +10,21 @@ public class Q4673 {
 //        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         int[] numbers = new int[10000];
-        for(int i = 1; i < 10000; i++) {
+        for (int i = 1; i < 10000; i++) {
             numbers[i] = i;
         }
-        for(int i = 1; i < 10000; i++) {
+        for (int i = 1; i < 10000; i++) {
             String[] splitNumber = String.valueOf(i).split("");
             int sum = i;
-            for(int j = 0 ; j < splitNumber.length; j++) {
+            for (int j = 0; j < splitNumber.length; j++) {
                 sum += Integer.parseInt(splitNumber[j]);
             }
-            if(sum < 10000) {
+            if (sum < 10000) {
                 numbers[sum] = 0;
             }
         }
-        for(int i = 0; i < numbers.length; i++) {
-            if(numbers[i] != 0) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] != 0) {
                 writer.append(numbers[i] + "\n");
             }
         }
