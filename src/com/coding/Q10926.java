78e6a171b9ebcb649ba674299d3b5c38300583aa
SeungWonKwon <loveyoo0103@naver.com>
2022-01-28 17:00:29 +0900

    Q10926, Q18108 불기년도, ??!

---
+++ src/com/coding/Q10926.java
@@ -0,0 +1,16 @@
+package com.coding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//??!
+public class Q10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        System.out.println(reader.readLine() + "??!");
+
+        reader.close();
+    }
+}
---
+++ src/com/coding/Q18108.java
@@ -0,0 +1,19 @@
+package com.coding;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+//1998년생인 내가 태국에서는 2541년생?!
+public class Q18108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        writer.append(String.valueOf(Integer.parseInt(reader.readLine()) - 543));
+
+        writer.flush();
+        reader.close();
+    }
+}
