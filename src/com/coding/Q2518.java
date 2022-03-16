f111c3dbf5e0ba56e13203bba622d7a48569f79e
SeungWonKwon <loveyoo0103@naver.com>
2022-03-16 18:00:50 +0900

    Q2518 소수

---
+++ src/com/coding/Q2518.java
@@ -0,0 +1,45 @@
+package com.coding;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+//소수
+public class Q2518 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        int start = Integer.parseInt(reader.readLine());
+        int end  = Integer.parseInt(reader.readLine());
+        int min = 0;
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            if(i == 2) {
+                sum += 2;
+                min = 2;
+                continue;
+            }
+            for(int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    break;
+                } else if(j + 1 == i) {
+                    if (sum == 0) {
+                        min = j + 1;
+                    }
+                    sum += j + 1;
+                }
+            }
+        }
+        if (sum == 0) {
+            writer.append("-1");
+        } else {
+            writer.append(String.valueOf(sum)).append("\n");
+            writer.append(String.valueOf(min));
+        }
+
+        writer.flush();
+        reader.close();
+        writer.close();
+    }
+}
