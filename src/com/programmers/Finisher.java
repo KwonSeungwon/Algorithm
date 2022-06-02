83aa64f3ca1fc96218293d71a3f2a3df7d9c8fed
Seungwon Kwon <loveyoo0103@naver.com>
2022-06-02 22:23:18 +0900

    프로그래머스 완주못한사람 풀이

--- src/com/programmers/Finisher.java
+++ src/com/programmers/Finisher.java
@@ -1,38 +1,18 @@
 package com.programmers;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class Finisher {
     public String solution(String[] participant, String[] completion) {
-//        List<String> participantToList = Arrays.stream(participant).collect(Collectors.toList());
-//        List<String> completionToList = Arrays.stream(completion).collect(Collectors.toList());
-//        for(String a : completionToList) {
-//            if(participantToList.contains(a)) {
-//                participantToList.remove(a);
-//            }
-//        }
-        String answer = "";
-//        HashMap<String, String> complete = new HashMap<>();
-//        for(String a : completion) {
-//            complete.put(a, a);
-//        }
-//        for(String a : participant) {
-//            if (complete.get(a) != null) {
-//                complete.remove(a);
-//            } else {
-//                answer = a;
-//            }
-//        }
-        String partic = Arrays.toString(participant);
-        for(String a : completion) {
-            partic = partic.replaceFirst(a, "");
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i = 0;
+        for(i = 0; i < completion.length;i++) {
+            if(!participant[i].equals(completion[i])) {
+                break;
+            }
         }
-        answer = partic.replace(",","");
-        answer = answer.replace("[","");
-        answer = answer.replace("]","").trim();
-        return answer;
+        return participant[i];
     }
 }
