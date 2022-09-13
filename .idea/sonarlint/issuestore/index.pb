1273ff5a2c90c791d19b389ac3de82bfec89608a
KwonSeungwon <loveyoo0103@naver.com>
2022-09-13 17:50:14 +0900

    백준 Q10815숫자카드

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,5 @@ K
 src/com/baekjoon/Q2525.java,c\c\ccbdd3b8aa3e8cf048a98bf660cf87d6860729a2
 P
  src/com/programmers/Pokemon.java,0\2\020ad7bf816f98dbaf865c96f3c1530805db2d63
+S
+#src/com/programmers/SquareRoot.java,9\4\94ff4f2c0bbc2ced69d5240d6440c3b7260de791
--- src/com/baekjoon/Q10815.java
+++ src/com/baekjoon/Q10815.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 //숫자 카드
 public class Q10815 {
@@ -15,20 +12,21 @@ public class Q10815 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         reader.readLine();
-        StringTokenizer hadTokenizer = new StringTokenizer(reader.readLine());
-        int cardCount = Integer.parseInt(reader.readLine());
-        StringTokenizer targetTokenizer = new StringTokenizer(reader.readLine());
-        Map<String, Integer> map = new HashMap<>();
-        while (targetTokenizer.hasMoreTokens()) {
-            map.put(targetTokenizer.nextToken(), 0);
+        StringTokenizer had = new StringTokenizer(reader.readLine());
+        Set<Integer> set = new HashSet<>();
+        while (had.hasMoreTokens()) {
+            set.add(Integer.parseInt(had.nextToken()));
         }
-        while(hadTokenizer.hasMoreTokens()) {
-            if (map.get(hadTokenizer.nextToken()) != null) {
-                System.out.println("in");
-                map.replace(hadTokenizer.nextToken(), 1);
+        reader.readLine();
+        StringTokenizer target = new StringTokenizer(reader.readLine());
+        while (target.hasMoreTokens()) {
+            if (set.contains(Integer.parseInt(target.nextToken()))) {
+                writer.append("1 ");
+            } else {
+                writer.append("0 ");
             }
         }
-        'sader.close();qq0
+        reader.close();
         writer.flush();
         writer.close();
     }
