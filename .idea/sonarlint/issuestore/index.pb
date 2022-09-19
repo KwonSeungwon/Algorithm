743db1a37b32b578b9580a7d35b42d867472869d
KwonSeungwon <loveyoo0103@naver.com>
2022-09-19 17:55:43 +0900

    프로그래머스 문자열만들기

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,7 @@ P
  src/com/programmers/Pokemon.java,0\2\020ad7bf816f98dbaf865c96f3c1530805db2d63
 S
 #src/com/programmers/SquareRoot.java,9\4\94ff4f2c0bbc2ced69d5240d6440c3b7260de791
+K
+src/com/baekjoon/Q3003.java,c\0\c0058c2063b132db8941285dea81671723ad9fee
+L
+src/com/baekjoon/Q25304.java,9\9\99c22f02a596381d922534e6668ff8683b7058cb
---
+++ src/com/programmers/JadenCase.java
@@ -0,0 +1,14 @@
+package com.programmers;
+
+public class JadenCase {
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        String splitStrings[] = s.split(" ");
+        for (String word : splitStrings) {
+            if (word.charAt(0) != ' ' && word.charAt(0) > 65) {
+
+            }
+        }
+        return answer.toString();
+    }
+}
