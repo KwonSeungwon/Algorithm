f7197375c6b204d697559eb7f322e9dd885622a1
KwonSeungwon <loveyoo0103@naver.com>
2022-09-20 17:17:13 +0900

    프로그래머스 JadenCase

--- src/com/programmers/JadenCase.java
+++ src/com/programmers/JadenCase.java
@@ -1,14 +1,42 @@
 package com.programmers;
 
+import java.util.StringTokenizer;
+
 public class JadenCase {
     public String solution(String s) {
         StringBuilder answer = new StringBuilder();
-        String splitStrings[] = s.split(" ");
-        for (String word : splitStrings) {
-            if (word.charAt(0) != ' ' && word.charAt(0) > 65) {
-
+        s = s.toLowerCase();
+        String[] splitWords = s.split("");
+        String agoToken = null;
+        for(String word : splitWords) {
+            String thisWord = word;
+            if (agoToken == null || agoToken.equals(" ")) {
+                if (!Character.isDigit(thisWord.charAt(0))) {
+                    answer.append(thisWord.toUpperCase());
+                } else {
+                    answer.append(thisWord);
+                }
+            } else {
+                answer.append(thisWord);
             }
+            agoToken = thisWord;
         }
         return answer.toString();
     }
+    // 깔끔한 풀이(숫자도 걍 바꿈)
+    /*
+      String answer = "";
+        String[] sp = s.toLowerCase().split("");
+        boolean flag = true;
+
+        for(String ss : sp) {
+            answer += flag ? ss.toUpperCase() : ss;
+            flag = ss.equals(" ") ? true : false;
+        }
+
+        return answer;
+  }
+     */
+
+
 }
--- src/com/programmers/Main.java
+++ src/com/programmers/Main.java
@@ -37,7 +37,7 @@ public class Main {
 //        new MockTest().solution(new int[]{1,3,2,4,2, 5, 7, 2, 8, 9 ,10 ,11});
 //        new BusinessCard().solution(new int[][]{{60, 50}, {30, 70}, {60, 30}, {80, 40}});
 //        new WordSorting().solution(new String[]{"abce", "abcd", "cdx"}, 2);
-        new JadenCase().solution("3people unFollowed me");
+        new JadenCase().solution("for the last week");
     }
 
 }
