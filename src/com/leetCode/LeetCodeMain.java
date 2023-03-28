6127e6d9207b2a99bc219e33412870e516d604be
KwonSeungwon <loveyoo0103@naver.com>
2023-03-28 18:19:49 +0900

    릿코드 미들오프링크드리스트

--- src/com/leetCode/LeetCodeMain.java
+++ src/com/leetCode/LeetCodeMain.java
@@ -6,7 +6,8 @@ public class LeetCodeMain {
 //        new SumOfOneArray().runningSum(new int[]{1,2,3,4});
 //        new RichestCustomerWealth().maximumWealth(new int[][]{{1,5},{7,3},{3,5}});
 //        new FizzBuzz().fizzBuzz(15);
-        new ReduceNumber().numberOfSteps(123);
+//        new ReduceNumber().numberOfSteps(123);
+        new LinkedList().middleNode(new LinkedList.ListNode());
 
     }
 }
---
+++ src/com/leetCode/LinkedList.java
@@ -0,0 +1,19 @@
+package com.leetCode;
+
+public class LinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode node = head;
+        node.next =
+        System.out.println(node.val);
+        return null;
+    }
+
+
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
