c352434173f8b85d066b872cfcca42c0ee21ab9f
KwonSeungwon <loveyoo0103@naver.com>
2023-04-17 18:04:43 +0900

    릿코드 푸는중

--- src/com/leetCode/LeetCodeMain.java
+++ src/com/leetCode/LeetCodeMain.java
@@ -1,5 +1,9 @@
 package com.leetCode;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class LeetCodeMain {
     public static void main(String[] args) {
 
@@ -13,7 +17,18 @@ public class LeetCodeMain {
 //        new TwoSum().twoSum(new int[]{2,7,11,15}, 9);
 //        new CoomonPrefix().longestCommonPrefix(new String[]{"dog","racecar","car"});
 //        new Brackets().isValid("(]");
-        new AddNumberOne().plusOne(new int[]{9,8,7,6,5,4,3,2,1,0});
+//        new AddNumberOne().plusOne(new int[]{7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6});
+//        new AddNumberOne().romanToInt("IV");
+
+        new LeetCodeMain().reformatDate("6th Jun 1933");
+    }
+
+
+    public String reformatDate(String date) {
+        String[] dates = date.split(" ");
+        dates[0] = dates[0].replaceAll("[^0-9]", "");
+        LocalDate localDate = LocalDate.parse(dates[0] + dates[1] + dates[2], DateTimeFormatter.ofPattern("ddMMMyyyy", Locale.ENGLISH));
+        return localDate.toString();
 
     }
 }
